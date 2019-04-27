105b178f8a1764c1494c4c0777b7f441dd1a6d43
vonHimik <assassiny1244@gmail.com>
2019-04-27 11:57:57 +0200

    Commit #1

---
+++ Assets/Scripts/CardNames.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardNames : MonoBehaviour
+{
+    public enum Name   // List of unit names
+    {
+        None,
+        Gul,
+        SkeletonArcher,
+        Dragon
+    }
+}
