6c89c5a538149605b64d9b888a86749981ae429f
Banzi <kocur.jan@seznam.cz>
2019-04-28 19:48:38 +0200

    Level chagner.

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+
+    private string levelToLoad;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FadeToLevel(string name) {
+        animator.SetTrigger("FadeOut");
+        levelToLoad = name;
+    }
+
+    public void OnFadeComplete() {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
